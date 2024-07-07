a1dfacb0c634fb77403b8d0050adeeb8f20a8e03
alexnicita <alexandernicita@gmail.com>
2024-07-07 16:51:23 -0400

    polymarket api requests are clean. nice liam

--- api/polymarket.py
+++ api/polymarket.py
@@ -1,3 +1,44 @@
 # core polymarket api
 # https://github.com/Polymarket/py-clob-client/tree/main/examples
 
+import os
+
+from py_clob_client.client import ClobClient
+from py_clob_client.clob_types import ApiCreds
+from dotenv import load_dotenv
+from py_clob_client.constants import AMOY
+
+load_dotenv()
+
+def test():
+    host = "https://clob.polymarket.com"
+    key = os.getenv("PK")
+    print(key)
+    chain_id = POLYGON
+
+    # Create CLOB client and get/set API credentials
+    client = ClobClient(host, key=key, chain_id=chain_id)
+    client.set_api_creds(client.create_or_derive_api_creds())
+
+
+def main():
+    host = "http://localhost:8080"
+    key = os.getenv("PK")
+    creds = ApiCreds(
+        api_key=os.getenv("CLOB_API_KEY"),
+        api_secret=os.getenv("CLOB_SECRET"),
+        api_passphrase=os.getenv("CLOB_PASS_PHRASE"),
+    ) # TODO: post /auth/api-key
+    chain_id = AMOY
+    client = ClobClient(host, key=key, chain_id=chain_id, creds=creds)
+
+    print(client.get_markets())
+    print(client.get_simplified_markets())
+    print(client.get_sampling_markets())
+    print(client.get_sampling_simplified_markets())
+    print(client.get_market("condition_id"))
+
+    print("Done!")
+
+
+main()
