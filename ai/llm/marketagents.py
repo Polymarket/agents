f8b5fcb7cb093a91b1f79a26b3eb686c27f99cd3
andrew65gs <andrew@greenestreet.xyz>
2024-07-15 11:53:51 -0400

    env setup 2

--- ai/llm/marketagents.py
+++ ai/llm/marketagents.py
@@ -4,19 +4,6 @@ from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain_openai import OpenAI
 
-# from langchain_openai import ChatOpenAI
-# from langchain_community.tools.tavily_search import TavilySearchResults
-# from langchain_core.messages import HumanMessage
-# from langgraph.checkpoint.sqlite import SqliteSaver
-# from langgraph.prebuilt import create_react_agent
-
-# Create the agent
-# memory = SqliteSaver.from_conn_string(":memory:")
-# model = ChatOpenAI(model_name="gpt-4")
-# search = TavilySearchResults(max_results=2)
-# tools = [search]
-# agent_executor = create_react_agent(model, tools, checkpointer=memory)
-
 load_dotenv()
 
 openai_api_key = os.getenv("OPEN_API_KEY")
