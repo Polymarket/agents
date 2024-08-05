28a89bd1e71d306c461a6e6773436ab662e011a1
alexnicita <alexandernicita@gmail.com>
2024-08-05 10:26:59 -0400

    snake case contract initialization

--- connectors/polymarket.py
+++ connectors/polymarket.py
@@ -45,7 +45,7 @@ class Polymarket:
         self.neg_risk_exchange_address = "0xC5d563A36AE78145C45a50134d48A1215220f80a"
 
         # self._init_api_keys()
-        # self._init_approvals(True)
+        self._init_approvals(False)  # set to True first time running this!
 
     def _init_api_keys(self):
         self.client = ClobClient(
@@ -73,7 +73,7 @@ class Polymarket:
         web3 = Web3(Web3.HTTPProvider(rpc_url))
         web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-        nonce = web3.eth.getTransactionCount(pub_key)
+        nonce = web3.eth.get_transaction_count(pub_key)
 
         usdc = web3.eth.contract(address=usdc_address, abi=erc20_approve)
         ctf = web3.eth.contract(address=ctf_address, abi=erc1155_set_approval)
@@ -81,94 +81,94 @@ class Polymarket:
         # CTF Exchange
         raw_usdc_approve_txn = usdc.functions.approve(
             "0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E", int(MAX_INT, 0)
-        ).buildTransaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
+        ).build_transaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
         signed_usdc_approve_tx = web3.eth.account.sign_transaction(
             raw_usdc_approve_txn, private_key=priv_key
         )
         send_usdc_approve_tx = web3.eth.send_raw_transaction(
-            signed_usdc_approve_tx.rawTransaction
+            signed_usdc_approve_tx.raw_transaction
         )
         usdc_approve_tx_receipt = web3.eth.wait_for_transaction_receipt(
             send_usdc_approve_tx, 600
         )
         print(usdc_approve_tx_receipt)
 
-        nonce = web3.eth.getTransactionCount(pub_key)
+        nonce = web3.eth.get_transaction_count(pub_key)
 
         raw_ctf_approval_txn = ctf.functions.setApprovalForAll(
             "0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E", True
-        ).buildTransaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
+        ).build_transaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
         signed_ctf_approval_tx = web3.eth.account.sign_transaction(
             raw_ctf_approval_txn, private_key=priv_key
         )
         send_ctf_approval_tx = web3.eth.send_raw_transaction(
-            signed_ctf_approval_tx.rawTransaction
+            signed_ctf_approval_tx.raw_transaction
         )
         ctf_approval_tx_receipt = web3.eth.wait_for_transaction_receipt(
             send_ctf_approval_tx, 600
         )
         print(ctf_approval_tx_receipt)
 
-        nonce = web3.eth.getTransactionCount(pub_key)
+        nonce = web3.eth.get_transaction_count(pub_key)
 
         # Neg Risk CTF Exchange
         raw_usdc_approve_txn = usdc.functions.approve(
             "0xC5d563A36AE78145C45a50134d48A1215220f80a", int(MAX_INT, 0)
-        ).buildTransaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
+        ).build_transaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
         signed_usdc_approve_tx = web3.eth.account.sign_transaction(
             raw_usdc_approve_txn, private_key=priv_key
         )
         send_usdc_approve_tx = web3.eth.send_raw_transaction(
-            signed_usdc_approve_tx.rawTransaction
+            signed_usdc_approve_tx.raw_transaction
         )
         usdc_approve_tx_receipt = web3.eth.wait_for_transaction_receipt(
             send_usdc_approve_tx, 600
         )
         print(usdc_approve_tx_receipt)
 
-        nonce = web3.eth.getTransactionCount(pub_key)
+        nonce = web3.eth.get_transaction_count(pub_key)
 
         raw_ctf_approval_txn = ctf.functions.setApprovalForAll(
             "0xC5d563A36AE78145C45a50134d48A1215220f80a", True
-        ).buildTransaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
+        ).build_transaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
         signed_ctf_approval_tx = web3.eth.account.sign_transaction(
             raw_ctf_approval_txn, private_key=priv_key
         )
         send_ctf_approval_tx = web3.eth.send_raw_transaction(
-            signed_ctf_approval_tx.rawTransaction
+            signed_ctf_approval_tx.raw_transaction
         )
         ctf_approval_tx_receipt = web3.eth.wait_for_transaction_receipt(
             send_ctf_approval_tx, 600
         )
         print(ctf_approval_tx_receipt)
 
-        nonce = web3.eth.getTransactionCount(pub_key)
+        nonce = web3.eth.get_transaction_count(pub_key)
 
         # Neg Risk Adapter
         raw_usdc_approve_txn = usdc.functions.approve(
             "0xd91E80cF2E7be2e162c6513ceD06f1dD0dA35296", int(MAX_INT, 0)
-        ).buildTransaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
+        ).build_transaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
         signed_usdc_approve_tx = web3.eth.account.sign_transaction(
             raw_usdc_approve_txn, private_key=priv_key
         )
         send_usdc_approve_tx = web3.eth.send_raw_transaction(
-            signed_usdc_approve_tx.rawTransaction
+            signed_usdc_approve_tx.raw_transaction
         )
         usdc_approve_tx_receipt = web3.eth.wait_for_transaction_receipt(
             send_usdc_approve_tx, 600
         )
         print(usdc_approve_tx_receipt)
 
-        nonce = web3.eth.getTransactionCount(pub_key)
+        nonce = web3.eth.get_transaction_count(pub_key)
 
         raw_ctf_approval_txn = ctf.functions.setApprovalForAll(
             "0xd91E80cF2E7be2e162c6513ceD06f1dD0dA35296", True
-        ).buildTransaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
+        ).build_transaction({"chainId": chain_id, "from": pub_key, "nonce": nonce})
         signed_ctf_approval_tx = web3.eth.account.sign_transaction(
             raw_ctf_approval_txn, private_key=priv_key
         )
         send_ctf_approval_tx = web3.eth.send_raw_transaction(
-            signed_ctf_approval_tx.rawTransaction
+            signed_ctf_approval_tx.raw_transaction
         )
         ctf_approval_tx_receipt = web3.eth.wait_for_transaction_receipt(
             send_ctf_approval_tx, 600
